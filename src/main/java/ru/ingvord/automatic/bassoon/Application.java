d08becc6daaf17d6eba8af97d265f5b3993d21dd
Igor Khokhriakov <ingvord.mail@gmail.com>
2025-05-26 10:16:39 +0200

    Update Application.java

--- src/main/java/ru/ingvord/automatic/bassoon/Application.java
+++ src/main/java/ru/ingvord/automatic/bassoon/Application.java
@@ -33,16 +33,19 @@ public class Application {
                 .timeout(Duration.ofSeconds(60))
                 .build();
 
+        // using AI service to extract meta data and create native Java objects tree, see entities
         ElnExtractor elnExtractor = AiServices.create(ElnExtractor.class, model);
 
-//        ollama does not support tooling
+//        Alas ollama does not support tooling, but this can be used to "teach" AI to read from HDF5 for instance
 //        ElnParser parser = AiServices.builder(ElnParser.class)
 //                .chatLanguageModel(model)
 //                .tools(new Hdf5StorageTools())
 //                    .build();
 
+        // pure Java hdf5 writer
         Hdf5Writer hdf5Writer = new Hdf5Writer();
 
+        // do stuff
         String inputFileName = args[0];
         NXmonopdEntry extracted = elnExtractor.extract(Files.readString(Paths.get(inputFileName)));
         hdf5Writer.write(extracted, inputFileName);
